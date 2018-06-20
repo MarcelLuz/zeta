8b8a18c5606d002829e3f94a9bfc231c541d81e7
aullik <nicolaswehrle@gmx.net>
2018-06-20 15:12:06 +0200

    create failing test for GraphicalDslInstance formatter

---
+++ api/common/src/test/scala/de/htwg/zeta/common/format/model/GraphicalDslInstanceFormatTest.scala
@@ -0,0 +1,74 @@
+package de.htwg.zeta.common.format.model
+
+import de.htwg.zeta.common.format.project.AttributeFormat
+import de.htwg.zeta.common.format.project.AttributeTypeFormat
+import de.htwg.zeta.common.format.project.AttributeValueFormat
+import de.htwg.zeta.common.format.project.MethodFormat
+import org.scalatest.FreeSpec
+import org.scalatest.Matchers
+import play.api.libs.json.Json
+
+class GraphicalDslInstanceFormatTest extends FreeSpec with Matchers {
+  val attributeTypeFormat = new AttributeTypeFormat()
+  val attributeValueFormat = new AttributeValueFormat()
+  val attributeFormat = new AttributeFormat(attributeTypeFormat, attributeValueFormat)
+  val methodFormat = new MethodFormat(attributeTypeFormat)
+  val nodeFormat = new NodeFormat(attributeFormat, attributeValueFormat, methodFormat)
+  val edgeFormat = new EdgeFormat(attributeFormat, attributeValueFormat, methodFormat)
+  val format = new GraphicalDslInstanceFormat(nodeFormat, edgeFormat,attributeFormat, attributeValueFormat, methodFormat)
+
+  "Parse input" - {
+    "should work" in {
+      val inp =
+        """
+         |{
+         |	"name": "1",
+         |	"graphicalDslId": "543efc93-6501-4bcf-8fa1-15ea2444fc53",
+         |	"nodes": [{
+         |			"name": "cd4b689a-b166-40a3-b617-8ca49296e5b7",
+         |			"className": "Entity",
+         |			"outputEdgeNames": [],
+         |			"inputEdgeNames": [],
+         |			"attributes": [{
+         |					"name": "EAttr",
+         |					"globalUnique": false,
+         |					"localUnique": false,
+         |					"type": "String",
+         |					"default": {
+         |						"type": "String",
+         |						"value": ""
+         |					},
+         |					"constant": false,
+         |					"singleAssignment": false,
+         |					"expression": "",
+         |					"ordered": false,
+         |					"transient": false,
+         |					"id": "00000000-0000-0000-0000-0000f00b1a0b"
+         |				}
+         |			],
+         |			"attributeValues": {
+         |				"EAttr": {
+         |					"value": ["..."],
+         |					"type": "String"
+         |				}
+         |			},
+         |			"methods": []
+         |		}
+         |	],
+         |	"edges": [],
+         |	"attributes": [],
+         |	"attributeValues": {},
+         |	"methods": [],
+         |  "uiState": ""
+         |}
+        """.stripMargin
+
+      val result = Json.fromJson(Json.parse(inp))(format)
+
+      if(result.isError) println(result)
+
+      result.isSuccess shouldBe true
+    }
+  }
+
+}
